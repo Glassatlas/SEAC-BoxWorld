b10c2bd39e98625d0d23e0b7b5495c42572712d3
Puttatida Mahapattanakul <puttatidam@gmail.com>
2021-09-10 19:18:01 +0100

    Delete seac_boxworld-main directory

--- seac_boxworld-main/seac/video_test.py
+++
@@ -1,9 +0,0 @@
-import gym
-
-env = gym.make('CartPole-v0')
-env = gym.wrappers.Monitor(env, "recording", force=True)
-env.reset()
-for i in range(1000):
-    obs, rew, done, info = env.step(env.action_space.sample())
-    if done:
-        env.reset()
